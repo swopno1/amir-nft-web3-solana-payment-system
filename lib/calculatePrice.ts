d14c8f1d4fb50cec84a694b7572027d90b4ebb88
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-07-31 14:33:47 +0600

    Fixing errprs in price calculation module.

--- lib/calculatePrice.ts
+++ lib/calculatePrice.ts
@@ -4,13 +4,19 @@ import { products } from './products'
 
 export default function calculatePrice(query: ParsedUrlQuery): BigNumber {
   let amount = new BigNumber(0)
+  console.log('check-1', amount)
   for (let [id, quantity] of Object.entries(query)) {
     const product = products.find((p) => p.id === id)
     if (!product) continue
 
-    const price = product.priceUsd
-    const productQuantity = new BigNumber(quantity as string)
-    amount = amount.plus(productQuantity.multipliedBy(price))
+    const solPrice = product.priceSol
+    const usdPrice = product.priceUsd
+
+    const productSolQuantity = new BigNumber(quantity[0] as string) //string | string[] | undefined
+    const productUsdQuantity = new BigNumber(quantity[1] as string)
+
+    amount = amount.plus(productSolQuantity.multipliedBy(solPrice))
+    amount = amount.plus(productUsdQuantity.multipliedBy(usdPrice))
   }
 
   return amount
--- pages/api/makeTransaction.ts
+++ pages/api/makeTransaction.ts
@@ -1,23 +1,34 @@
-import { createTransferCheckedInstruction, getAssociatedTokenAddress, getMint, getOrCreateAssociatedTokenAccount } from "@solana/spl-token"
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
-import { clusterApiUrl, Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js"
-import { NextApiRequest, NextApiResponse } from "next"
-import { couponAddress, shopAddress, usdcAddress } from "../../lib/addresses"
-import calculatePrice from "../../lib/calculatePrice"
+import {
+  createTransferCheckedInstruction,
+  getAssociatedTokenAddress,
+  getMint,
+  getOrCreateAssociatedTokenAccount,
+} from '@solana/spl-token'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  PublicKey,
+  Transaction,
+} from '@solana/web3.js'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { couponAddress, shopAddress, usdcAddress } from '../../lib/addresses'
+import calculatePrice from '../../lib/calculatePrice'
 import base58 from 'bs58'
 
 export type MakeTransactionInputData = {
-  account: string,
+  account: string
 }
 
 type MakeTransactionGetResponse = {
-  label: string,
-  icon: string,
+  label: string
+  icon: string
 }
 
 export type MakeTransactionOutputData = {
-  transaction: string,
-  message: string,
+  transaction: string
+  message: string
 }
 
 type ErrorOutput = {
@@ -26,8 +37,8 @@ type ErrorOutput = {
 
 function get(res: NextApiResponse<MakeTransactionGetResponse>) {
   res.status(200).json({
-    label: "Cookies Inc",
-    icon: "https://freesvg.org/img/1370962427.png",
+    label: 'Amir NFT',
+    icon: 'https://freesvg.org/img/1370962427.png',
   })
 }
 
@@ -42,28 +53,29 @@ async function post(
       res.status(400).json({ error: "Can't checkout with charge of 0" })
       return
     }
+    console.log(req.query)
 
     // We pass the reference to use in the query
     const { reference } = req.query
     if (!reference) {
-      res.status(400).json({ error: "No reference provided" })
+      res.status(400).json({ error: 'No reference provided' })
       return
     }
 
     // We pass the buyer's public key in JSON body
     const { account } = req.body as MakeTransactionInputData
     if (!account) {
-      res.status(40).json({ error: "No account provided" })
+      res.status(40).json({ error: 'No account provided' })
       return
     }
 
     // We get the shop private key from .env - this is the same as in our script
     const shopPrivateKey = process.env.SHOP_PRIVATE_KEY as string
     if (!shopPrivateKey) {
-      res.status(500).json({ error: "Shop private key not available" })
+      res.status(500).json({ error: 'Shop private key not available' })
     }
-    const shopKeypair = Keypair.fromSecretKey(base58.decode(shopPrivateKey))
 
+    const shopKeypair = Keypair.fromSecretKey(base58.decode(shopPrivateKey))
     const buyerPublicKey = new PublicKey(account)
     const shopPublicKey = shopKeypair.publicKey
 
@@ -72,15 +84,18 @@ async function post(
     const connection = new Connection(endpoint)
 
     // Get the buyer and seller coupon token accounts
-    // Buyer one may not exist, so we create it (which costs SOL) as the shop account if it doesn't 
+    // Buyer one may not exist, so we create it (which costs SOL) as the shop account if it doesn't
     const buyerCouponAccount = await getOrCreateAssociatedTokenAccount(
       connection,
       shopKeypair, // shop pays the fee to create it
       couponAddress, // which token the account is for
-      buyerPublicKey, // who the token account belongs to (the buyer)
+      buyerPublicKey // who the token account belongs to (the buyer)
     )
 
-    const shopCouponAddress = await getAssociatedTokenAddress(couponAddress, shopPublicKey)
+    const shopCouponAddress = await getAssociatedTokenAddress(
+      couponAddress,
+      shopPublicKey
+    )
 
     // If the buyer has at least 5 coupons, they can use them and get a discount
     const buyerGetsCouponDiscount = buyerCouponAccount.amount >= 5
@@ -88,12 +103,18 @@ async function post(
     // Get details about the USDC token
     const usdcMint = await getMint(connection, usdcAddress)
     // Get the buyer's USDC token account address
-    const buyerUsdcAddress = await getAssociatedTokenAddress(usdcAddress, buyerPublicKey)
+    const buyerUsdcAddress = await getAssociatedTokenAddress(
+      usdcAddress,
+      buyerPublicKey
+    )
     // Get the shop's USDC token account address
-    const shopUsdcAddress = await getAssociatedTokenAddress(usdcAddress, shopPublicKey)
+    const shopUsdcAddress = await getAssociatedTokenAddress(
+      usdcAddress,
+      shopPublicKey
+    )
 
     // Get a recent blockhash to include in the transaction
-    const { blockhash } = await (connection.getLatestBlockhash('finalized'))
+    const { blockhash } = await connection.getLatestBlockhash('finalized')
 
     const transaction = new Transaction({
       recentBlockhash: blockhash,
@@ -110,8 +131,8 @@ async function post(
       usdcAddress, // mint (token address)
       shopUsdcAddress, // destination
       buyerPublicKey, // owner of source address
-      amountToPay.toNumber() * (10 ** usdcMint.decimals), // amount to transfer (in units of the USDC token)
-      usdcMint.decimals, // decimals of the USDC token
+      amountToPay.toNumber() * 10 ** usdcMint.decimals, // amount to transfer (in units of the USDC token)
+      usdcMint.decimals // decimals of the USDC token
     )
 
     // Add the reference to the instruction as a key
@@ -123,25 +144,25 @@ async function post(
     })
 
     // Create the instruction to send the coupon from the shop to the buyer
-    const couponInstruction = buyerGetsCouponDiscount ?
-      // The coupon instruction is to send 5 coupons from the buyer to the shop
-      createTransferCheckedInstruction(
-        buyerCouponAccount.address, // source account (coupons)
-        couponAddress, // token address (coupons)
-        shopCouponAddress, // destination account (coupons)
-        buyerPublicKey, // owner of source account
-        5, // amount to transfer
-        0, // decimals of the token - we know this is 0
-      ) :
-      // The coupon instruction is to send 1 coupon from the shop to the buyer
-      createTransferCheckedInstruction(
-        shopCouponAddress, // source account (coupon)
-        couponAddress, // token address (coupon)
-        buyerCouponAccount.address, // destination account (coupon)
-        shopPublicKey, // owner of source account
-        1, // amount to transfer
-        0, // decimals of the token - we know this is 0
-      )
+    const couponInstruction = buyerGetsCouponDiscount
+      ? // The coupon instruction is to send 5 coupons from the buyer to the shop
+        createTransferCheckedInstruction(
+          buyerCouponAccount.address, // source account (coupons)
+          couponAddress, // token address (coupons)
+          shopCouponAddress, // destination account (coupons)
+          buyerPublicKey, // owner of source account
+          5, // amount to transfer
+          0 // decimals of the token - we know this is 0
+        )
+      : // The coupon instruction is to send 1 coupon from the shop to the buyer
+        createTransferCheckedInstruction(
+          shopCouponAddress, // source account (coupon)
+          couponAddress, // token address (coupon)
+          buyerCouponAccount.address, // destination account (coupon)
+          shopPublicKey, // owner of source account
+          1, // amount to transfer
+          0 // decimals of the token - we know this is 0
+        )
 
     // Add the shop as a signer to the coupon instruction
     // If the shop is sending a coupon, it already will be a signer
@@ -163,13 +184,15 @@ async function post(
     // Serialize the transaction and convert to base64 to return it
     const serializedTransaction = transaction.serialize({
       // We will need the buyer to sign this transaction after it's returned to them
-      requireAllSignatures: false
+      requireAllSignatures: false,
     })
     const base64 = serializedTransaction.toString('base64')
 
     // Insert into database: reference, amount
 
-    const message = buyerGetsCouponDiscount ? "50% Discount! 🍪" : "Thanks for your order! 🍪"
+    const message = buyerGetsCouponDiscount
+      ? '50% Discount! 🍪'
+      : 'Thanks for your order! 🍪'
 
     // Return the serialized transaction
     res.status(200).json({
@@ -177,22 +200,24 @@ async function post(
       message,
     })
   } catch (err) {
-    console.error(err);
+    console.error(err)
 
-    res.status(500).json({ error: 'error creating transaction', })
+    res.status(500).json({ error: 'error creating transaction' })
     return
   }
 }
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<MakeTransactionGetResponse | MakeTransactionOutputData | ErrorOutput>
+  res: NextApiResponse<
+    MakeTransactionGetResponse | MakeTransactionOutputData | ErrorOutput
+  >
 ) {
-  if (req.method === "GET") {
+  if (req.method === 'GET') {
     return get(res)
-  } else if (req.method === "POST") {
+  } else if (req.method === 'POST') {
     return await post(req, res)
   } else {
-    return res.status(405).json({ error: "Method not allowed" })
+    return res.status(405).json({ error: 'Method not allowed' })
   }
 }
