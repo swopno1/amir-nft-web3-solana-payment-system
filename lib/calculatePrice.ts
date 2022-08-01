6668bdeb0a919039f4d66a06c803f7253aaecf45
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-08-01 09:24:45 +0600

    Fixed type issue in calculatePrice lib.

--- lib/calculatePrice.ts
+++ lib/calculatePrice.ts
@@ -4,16 +4,17 @@ import { products } from './products'
 
 export default function calculatePrice(query: ParsedUrlQuery): BigNumber {
   let amount = new BigNumber(0)
-  console.log('check-1', amount)
+
   for (let [id, quantity] of Object.entries(query)) {
     const product = products.find((p) => p.id === id)
     if (!product) continue
 
     const solPrice = product.priceSol
     const usdPrice = product.priceUsd
+    const [solQty, usdQty] = quantity as string[]
 
-    const productSolQuantity = new BigNumber(quantity[0] as string) //string | string[] | undefined
-    const productUsdQuantity = new BigNumber(quantity[1] as string)
+    const productSolQuantity = new BigNumber(solQty) //string | string[] | undefined
+    const productUsdQuantity = new BigNumber(usdQty)
 
     amount = amount.plus(productSolQuantity.multipliedBy(solPrice))
     amount = amount.plus(productUsdQuantity.multipliedBy(usdPrice))
