d12242c0c1f45e76c99673c1c0ad18e1de940327
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-08-01 09:43:16 +0600

    Change footer link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10">
+    <footer className="flex h-10 w-full justify-center border-t-2 border-gray-900 py-12">
       <a
-        href='https://www.amirnft.com/project'
-        target='_blank'
-        rel='noopener noreferrer'
+        href="https://www.amirnft.com/nfts"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        Check My web3 and NFT Projects at{" "}
-        <span>AmirNFT</span>
+        Check My web3 and NFT Projects at <span>AmirNFT</span>
       </a>
     </footer>
   )
